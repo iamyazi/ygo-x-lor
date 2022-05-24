891be6cab52d87e782fff93edd49fc417f297219
sammy <ar.sammy.01@gmail.com>
2022-05-24 14:03:50 +0100

    experimenter script update

--- script/c2006.lua
+++ script/c2006.lua
@@ -2,30 +2,36 @@
 local s,id=GetID()
 function s.initial_effect(c)
     c:SetUniqueOnField(1,0,id)
-    --search axes
+    --trib summ eff
     local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
-
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local h=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-		return h<3 and Duel.IsPlayerCanDraw(tp,3-h)
-	end
-	local h=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,3) end
 	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(3-h)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3-h)
+	Duel.SetTargetParam(3)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local h=Duel.GetFieldGroupCount(p,LOCATION_HAND,0)
-	if h>=3 then return end
-	Duel.Draw(p,3-h,REASON_EFFECT)
+	local hg=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+	discard=Duel.SendtoGrave(hg,REASON_EFFECT+REASON_DISCARD)
+	draw=Duel.Draw(tp, 3, REASON_EFFECT)
+	if discard>0 and draw>0 then
+		Duel.BreakEffect()
+		if #g>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local dg=g:Select(tp,1,1,nil)
+			Duel.HintSelection(dg,true)
+			Duel.Destroy(dg,REASON_EFFECT)
+		end
+		Duel.Damage(1-tp,#hg*600,REASON_EFFECT)
+	end
 end

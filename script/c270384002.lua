0200ef93fa8e85f5c17ab064042db335cbe5c49c
yazi <ar.sammy.01@gmail.com>
2022-05-26 02:45:50 +0100

    added mists call, escaped abom token, and cursed keeper. half coded keeper and updated banlist to include the new cards. removed call of the haunted from the whitelist.

---
+++ script/c270384002.lua
@@ -0,0 +1,41 @@
+--Cursed Keeper
+local s,id=GetID()
+function s.initial_effect(c)
+    --destruction eff
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_TO_GRAVE)
+    e1:SetCondition(s.condition)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
+    c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_DESTROY)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,270384001,0,TYPES_TOKEN,2000,0,1,RACE_FIEND,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,270384001,0,TYPES_TOKEN,2000,0,1,RACE_FIEND,ATTRIBUTE_DARK) then
+		local token=Duel.CreateToken(tp,270384001)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		--Cannot be tributed for a tribute summon
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3304)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e1,true)
+		Duel.SpecialSummonComplete()
+	end
+end

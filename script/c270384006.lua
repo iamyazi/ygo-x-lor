cef0164413c3dadcaa9a51f123d04c86e266c2b5
yazi <ar.sammy.01@gmail.com>
2022-06-01 00:49:54 +0100

    added allure and terraforming to whitelist. added fading icon

---
+++ script/c270384006.lua
@@ -0,0 +1,32 @@
+--fading icon
+local s,id=GetID()
+function s.initial_effect(c)
+	--summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	--e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,270384005,0,TYPES_TOKEN,500,500,1,RACE_FAIRY,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,270384005,0,TYPES_TOKEN,500,500,1,RACE_FAIRY,ATTRIBUTE_DARK) then
+		local token=Duel.CreateToken(tp,270384005)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

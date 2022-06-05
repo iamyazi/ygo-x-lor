183b56e6e3c575d6aed3eb34d81a0d4e7bdd352e
yazi <ar.sammy.01@gmail.com>
2022-06-05 14:14:26 +0100

    FIXED AGAIN

--- script/c270381000.lua
+++ script/c270381000.lua
@@ -48,7 +48,7 @@ end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil)

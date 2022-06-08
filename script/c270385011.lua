76e58c912b89d2d6a053024971bb006d54cc63c6
yazi <ar.sammy.01@gmail.com>
2022-06-09 00:54:03 +0100

    added gallant rider

--- script/c270385011.lua
+++ script/c270385011.lua
@@ -2,7 +2,101 @@
 local s,id=GetID()
 function s.initial_effect(c)
     --Xyz summon
-    Xyz.AddProcedure(c,nil,4,2)
+    Xyz.AddProcedure(c,nil,4,2,nil,nil,99)
     c:EnableReviveLimit()
-    
+    --atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.val)
+    c:RegisterEffect(e1)
+    --defup
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_UPDATE_DEFENSE)
+    c:RegisterEffect(e2)
+	--equip
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_EQUIP)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+    --destroy
+	local e4=Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_DESTROY)
+    --e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+	e4:SetCondition(s.descon)
+	e4:SetCost(s.descost)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	c:RegisterEffect(e4)
+end
+function s.atkfilter(c)
+    return c:IsType(TYPE_EQUIP) and c:IsType(TYPE_SPELL) and c:IsFaceup()
+end
+function s.val(e,c)
+	return Duel.GetMatchingGroupCount(s.atkfilter,e:GetHandlerPlayer(),LOCATION_SZONE,0,nil)*300
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.filter(c,e,tp,ec)
+	return aux.HasListedSetCode(c,0x1388) and c:IsType(TYPE_EQUIP) and c:CheckEquipTarget(ec)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local c=e:GetHandler()
+    local mat=c:GetOverlayCount()
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,e,tp,c)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp,c) end
+	local ft=math.min(Duel.GetLocationCount(tp,LOCATION_SZONE),mat)
+	if chk==0 then return ft>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,e:GetHandler()) end
+	local eg=aux.SelectUnselectGroup(g,e,tp,1,ft,aux.dncheck,1,tp,HINTMSG_EQUIP)
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,eg,#eg,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	local g=Duel.GetTargetCards(e)
+	if ft<#g then return end
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		Duel.Equip(tp,tc,c,true,true)
+	end
+	Duel.EquipComplete()
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local loct=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+    return loct==LOCATION_MZONE and re:IsActiveType(TYPE_MONSTER)
+        and re:GetHandler():IsControler(tp) and re:GetHandler():IsSetCard(0x1388)
+        and e:GetHandler():GetOverlayGroup():IsExists(Card.IsSetCard,1,nil,0x1388)
+        --and not e:GetHandler():IsStatus(STATUS_CHAINING)
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	c:RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil) end
+    local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,nil)
+    if #dg>0 then
+        Duel.Destroy(dg,REASON_EFFECT)
+    end
 end

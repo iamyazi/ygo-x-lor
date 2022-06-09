6a5e18cb093c4dbe77952df93efde825a8485a2f
yazi <ar.sammy.01@gmail.com>
2022-06-09 01:48:08 +0100

    added chain vest

---
+++ script/c270385012.lua
@@ -0,0 +1,61 @@
+--Chain Vest
+local s,id=GetID()
+function s.initial_effect(c)
+    aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsSetCard,0x1388))
+    --indestructable by battle once
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetValue(s.valcon)
+	e1:SetCountLimit(1)
+	c:RegisterEffect(e1)
+    --add back
+    local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	--register
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_EQUIP)
+	e3:SetOperation(s.resetop)
+	e3:SetRange(LOCATION_SZONE)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x1388}
+function s.valcon(e,re,r,rp)
+	return r==REASON_BATTLE
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_WARRIOR) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,c)
+	end
+end
+function s.resetop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not eg:IsContains(c) or not c:IsLocation(LOCATION_SZONE) then return end
+	if c:GetFlagEffect(id)==0 then
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,0)
+	end
+end

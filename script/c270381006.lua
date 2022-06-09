1b48ed17aebc88865c097d928fbd98dd4909bbf6
yazi <ar.sammy.01@gmail.com>
2022-06-09 22:02:04 +0100

    scorched added + i did some stuff i think i forgor tbh

---
+++ script/c270381006.lua
@@ -0,0 +1,45 @@
+--scorched earth
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+    return c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.filter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+		if Duel.Destroy(tc,REASON_EFFECT)==0 then return end
+    end
+    local zone=(2^tc:GetPreviousSequence())<<8
+    if tc:IsControler(tp) then
+        zone=zone
+    else
+        zone=zone<<16
+    end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_DISABLE_FIELD)
+    e1:SetOperation(s.disop)
+    e1:SetLabel(zone)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.disop(e,tp)
+	return e:GetLabel()
+end

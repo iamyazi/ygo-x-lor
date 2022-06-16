cb1dc5d48c92fc434e9601cd7951404cd38ab680
yazi <ar.sammy.01@gmail.com>
2022-06-16 14:17:08 +0100

    reworked spirit journey (needs ruling testing).

--- script/c270384013.lua
+++ script/c270384013.lua
@@ -17,11 +17,29 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+    
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) then
-        Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP_DEFENSE)
+    if tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) then
+        tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+        local e1=Effect.CreateEffect(c)
+        e1:SetDescription(aux.Stringid(id,1))
+        e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+        e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+        e1:SetCode(EVENT_PHASE+PHASE_END)
+        e1:SetLabelObject(tc)
+        e1:SetCountLimit(1)
+        e1:SetCondition(s.retcon)
+        e1:SetOperation(s.sumop)
+        Duel.RegisterEffect(e1,tp)  
     end
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetFlagEffect(id)~=0
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.SpecialSummon(e:GetLabelObject(),0,tp,tp,false,false,POS_FACEUP)
 end

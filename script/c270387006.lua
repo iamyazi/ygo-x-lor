da162b38371c5fe947aefe15a4eb960cdd61b054
yazi <ar.sammy.01@gmail.com>
2022-06-20 21:30:27 +0100

    added eye of the dragon trolldespair.

---
+++ script/c270387006.lua
@@ -0,0 +1,111 @@
+--Eye of the Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--to defense
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.potg)
+	e1:SetOperation(s.poop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+	--count spells
+    aux.GlobalCheck(s,function()
+        local e3=Effect.CreateEffect(c)
+        e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e3:SetCode(EVENT_CHAINING)
+        e3:SetOperation(s.regop)
+        Duel.RegisterEffect(e3,0)
+    end)
+    --sp dragonling
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.spcon)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+end
+function s.potg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return e:GetHandler():IsAttackPos() end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,e:GetHandler(),1,0,0)
+end
+function s.poop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsAttackPos() and c:IsRelateToEffect(e) then
+		Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	end
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+    if re:IsActiveType(TYPE_SPELL) then
+        Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,2)
+    end
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+    return Duel.GetFlagEffect(tp,id)>=2
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,1200,0,2,RACE_DRAGON,ATTRIBUTE_WATER) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,1200,0,2,RACE_DRAGON,ATTRIBUTE_WATER) then
+		local token=Duel.CreateToken(tp,id+1)
+        Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+        --dies during end
+        local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetOperation(s.desop)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		token:RegisterEffect(e1,true)
+        --Opponent can only target this card for attacks
+        local e2=Effect.CreateEffect(e:GetHandler())
+        e2:SetType(EFFECT_TYPE_FIELD)
+        e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+        e2:SetRange(LOCATION_MZONE)
+        e2:SetTargetRange(0,LOCATION_MZONE)
+        e2:SetValue(s.atlimit)
+        token:RegisterEffect(e2,true)
+        --lifesteal
+        local e4=Effect.CreateEffect(e:GetHandler())
+        e4:SetDescription(aux.Stringid(id,1))
+        e4:SetCategory(CATEGORY_DAMAGE+CATEGORY_TOGRAVE)
+        e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+        e4:SetCode(EVENT_BATTLED)
+        e4:SetTarget(s.tgtg)
+        e4:SetOperation(s.tgop)
+        token:RegisterEffect(e4,true)
+        Duel.SpecialSummonComplete()
+	end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end
+function s.atlimit(e,c)
+	return c~=e:GetHandler()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,e:GetHandler():GetAttack())
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() and c:GetAttack()>0 then
+		Duel.Recover(tp,c:GetAttack(),REASON_EFFECT)
+	end
+end

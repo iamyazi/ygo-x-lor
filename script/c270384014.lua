9936b35851dac57253746598a52ce72def090fd8
yazi <ar.sammy.01@gmail.com>
2022-07-02 00:14:12 +0100

    bugfixed crumble to no longer target itself.

--- script/c270384014.lua
+++ script/c270384014.lua
@@ -12,18 +12,18 @@ function s.initial_effect(c)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)	
 end
-function s.cfilter(c,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsFaceup()
+function s.filter1(c,tp)
+    return c:IsFaceup() and Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c) 
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    if chkc then return false end
+    if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,tp) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELF)
     local g1=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
     e:SetLabelObject(g1:GetFirst())
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPPO)
-    local g2=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local gr=Group.FromCards(g1:GetFirst(),e:GetHandler())
+    local g2=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,gr)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,1,0,0)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g2,1,0,0)
 end
@@ -32,7 +32,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=g:GetFirst()
     local sc=g:GetNext()
     if sc==e:GetLabelObject() then tc,sc=sc,tc end
-    if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 and sc:IsRelateToEffect(e) then
+    if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.Destroy(tc,REASON_EFFECT)~=0 and sc:IsRelateToEffect(e) then
         Duel.Destroy(sc, REASON_EFFECT)
     end
 end

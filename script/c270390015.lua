a5d95ea7bd2ebf399902d1fa9423d802a42d3f54
yazi <ar.sammy.01@gmail.com>
2022-10-10 22:36:49 +0100

    bugfixing behold the infinite for the 100th time

--- script/c270390015.lua
+++ script/c270390015.lua
@@ -14,27 +14,29 @@ end
 s.listed_series={0x384}
 function s.cfilter(c,e,tp)
 	local lv=c:GetLevel()
-	return lv>0 and c:IsFaceup() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,lv,e,tp)
+	local hg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_HAND,0,nil,e,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return lv>0 and c:IsFaceup() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,lv,hg,ft,e,tp)
 end
-function s.spfilter(c,lv,e,tp)
-	return c:IsLevelBelow(lv) and c:IsSetCard(0x384) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false)
+function s.spfilter(c,lv,hg,ft,e,tp)
+	return hg:CheckWithSumEqual(Card.GetLevel,lv,1,ft) and c:IsSetCard(0x384) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.cfilter(chkc,e,tp) end
 	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
+	local slv=tc:GetLevel()
 	if tc:IsRelateToEffect(e) and Duel.Release(tc,REASON_EFFECT)>0 then
 		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 		if ft<=0 then return end
 		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-		local slv=tc:GetLevel()
-		local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,slv,e,tp)
+		local hg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_HAND,0,nil,e,tp)
+		local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,slv,hg,ft,e,tp)
 		if #sg==0 then return end
 		local tg=sg:SelectWithSumEqual(tp,Card.GetLevel,slv,1,ft)
 		Duel.SpecialSummon(tg,SUMMON_TYPE_RITUAL,tp,tp,true,false,POS_FACEUP)

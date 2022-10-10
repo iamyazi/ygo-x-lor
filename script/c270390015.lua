08e749e8df2d1babf3916aafd86eef3f248c5578
yazi <ar.sammy.01@gmail.com>
2022-10-10 16:33:40 +0100

    fixed pool shark i think

--- script/c270390015.lua
+++ script/c270390015.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-
+s.listed_series={0x384}
 function s.spfilter(c,e,tp,rg,ft)
 	return c:IsSetCard(0x384) and c:IsCanBeSpecialSummoned(e,0,tp,false,true) and rg:CheckWithSumEqual(Card.GetLevel,lv,1,ft)
 end
